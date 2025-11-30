de67c30214436244efd05c05217d1603fe6da79f
Eduardo Silva <silvapereira1608gb@gmail.com>
2025-11-29 21:41:59 -0300

    feat: dados pessoais inseridos dinamicamente pelo fetch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,35 @@
+function updateProfileInfo(profileData) {
+  // DECLARANDO VARIAVEIS
+  const photo = document.getElementById("profile.photo");
+  const name = document.getElementById("profile.name");
+  const job = document.getElementById("profile.job");
+  const location = document.getElementById("profile.location");
+  const phone = document.getElementById("profile.phone");
+  const email = document.getElementById("profile.email");
+
+  // ALTERANDO FOTO
+  photo.src = profileData.photo;
+  photo.alt = profileData.name;
+
+  // ALTERANDO NOME
+  name.innerText = profileData.name;
+
+  // ALTERANDO CARGO
+  job.innerText = profileData.job;
+
+  // ALTERANDO LOCALIZAÇÃO
+  location.innerText = profileData.location;
+
+  // ALTERANDO TELFONE
+  phone.innerText = profileData.phone;
+  phone.href = `tel: +55 ${profileData.phone}`;
+
+  // ALTERANDO email
+  email.innerText = profileData.email;
+  email.href = `mailto: ${profileData.email}`;
+}
+
 (async () => {
   const profileData = await fetchProfileData();
-  console.log(profileData);
+  updateProfileInfo(profileData);
 })();
